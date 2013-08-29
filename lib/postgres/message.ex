9f6c1c5f702e8ca6dbd6d683deed9e6c2258d07a
Joshua Scott <joshua.scott@gmail.com>
2013-08-29 10:05:08 -0500

    use automatic binary to integer conversion

--- lib/postgres/message.ex
+++ lib/postgres/message.ex
@@ -3,37 +3,20 @@ defmodule Postgres.Message do
 
   @doc "Query"
   def query(query_string) do
-    "Q" <> int_to_string(32, String.length(query_string) + 5) <> cstring(query_string)
+    <<"Q", String.length(query_string) + 5 :: 32, cstring(query_string) :: binary >>
   end
 
   @doc "StartupMessage"
   def startup_message(user, database) do
-    message = cstring("user") <> cstring(user) <> cstring("database") <> cstring(database) <> "\0"
-    int_to_string(32, String.length(message) + 8) <> int_to_string(32, 196608) <> message
+    message = <<cstring("user") :: binary,
+      cstring(user) :: binary,
+      cstring("database") :: binary,
+      cstring(database) :: binary,
+      "\0" >>
+    << String.length(message) + 8 :: 32, 196608 :: 32, message :: binary >>
   end
 
   defp cstring(string), do: <<string :: binary, 0>>
-
-  defp int_to_string(32, num) when num <= 2147483647, do: do_int_to_string(8, num)
-  defp int_to_string(16, num) when num <= 32767, do: do_int_to_string(4, num)
-  defp int_to_string(8, num) when num <= 127, do: do_int_to_string(2, num)
-  defp int_to_string(bits, _num) when bits == 32 or bits == 16 or bits == 8, do: ArgumentError[message: "Length too large"]
-  defp int_to_string(_bits, _num), do: ArgumentError[message: "Bad bitsize"]
-
-  defp do_int_to_string(size, num) do
-    num |>
-      integer_to_binary(16) |>
-      String.rjust(size, ?0) |> 
-      binary_to_intstring
-  end
-
-  defp binary_to_intstring(str), do: binary_to_intstring("", str)
-  defp binary_to_intstring(str, ""), do: str
-  defp binary_to_intstring(str, <<a, b, t :: binary>>) do
-    num = binary_to_integer(<<a, b>>, 16)
-    <<str :: binary, num>> |> binary_to_intstring(t)
-  end
-
 
 end
 
